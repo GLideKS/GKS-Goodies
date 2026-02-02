5b84a96988dfcbf9a061c1b3e40db17e851b4463
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-02-02 13:43:54 -0400

    Fix overtime changes not triggering if there's no time limit

--- src/Lua/Ringslinger/TimeLimit stuff.lua
+++ src/Lua/Ringslinger/TimeLimit stuff.lua
@@ -35,38 +35,35 @@ GoodiesHook.ThinkFrame.timelimitchanges = function()
 	if gamestate != GS_LEVEL then return end
 	if CBW_Battle then return end -- BattleMod has already this kind of stuff
 	if not gamemap then return end
-	if (gametyperules & GTR_RINGSLINGER|GTR_TIMELIMIT) and timelimit then
-		local ptsr_chance = P_RandomChance(FU/7) --around 14% chance. very low
-		--Low Time
-		if getRemainingTics() > 0 and (getRemainingTics() <= LowTime_Trigger
-		or ((redscore == pointlimit-1) or (bluescore == pointlimit-1)))
-		and not gd.lowtime then
-			local lowtmmus = lowtime_musics[P_RandomRange(1, #lowtime_musics)]
-			S_ChangeGlobalMusic(lowtmmus)
-			gd.lowtime = true
+	local ptsr_chance = P_RandomChance(FU/7) --around 14% chance. very low
+	--Low Time
+	if ((timelimit and (getRemainingTics() > 0 and (getRemainingTics() <= LowTime_Trigger)))
+	or (((redscore == pointlimit-1) or (bluescore == pointlimit-1)) and (gametyperules & GTR_TEAMFLAGS)))
+	and not gd.lowtime then
+		local lowtmmus = lowtime_musics[P_RandomRange(1, #lowtime_musics)]
+		S_ChangeGlobalMusic(lowtmmus)
+		gd.lowtime = true
+	end
+	--Overtime
+	if ((timelimit and getRemainingTics() == 0)
+	or ((redscore == pointlimit-1 and bluescore == pointlimit-1) and (gametyperules & GTR_TEAMFLAGS)))
+	and not gd.overtime then
+		if not ptsr_chance then
+			local ovtmmus = overtime_musics[P_RandomRange(1, #overtime_musics)]
+			S_ChangeGlobalMusic(ovtmmus, settings.overtime_weather, settings.overtime_sky)
+		else --hehe
+			S_StartSound(nil, sfx_litng2)
+			S_ChangeGlobalMusic("OTPTSR", nil, 34)
 		end
-		--Overtime
-		if ((getRemainingTics() == 0)
-		or ((redscore == pointlimit-1 and bluescore == pointlimit-1) and gametype == (GT_RSRCTF or GT_CTF)))
-		and not gd.overtime then
-			if not ptsr_chance then
-				local ovtmmus = overtime_musics[P_RandomRange(1, #overtime_musics)]
-				S_ChangeGlobalMusic(ovtmmus, settings.overtime_weather, settings.overtime_sky)
-			else --hehe
-				S_StartSound(nil, sfx_litng2)
-				S_ChangeGlobalMusic("OTPTSR", nil, 34)
-			end
-			gd.overtime = true
+		gd.overtime = true
+	end
+	--Did the player joined mid game? play the current music and weather going on
+	for p in players.iterate do
+		if gd.lowtime and S_MusicName(p) != gd.currentmusicplaying then
+			S_ChangeGlobalMusic(gd.currentmusicplaying)
 		end
-
-		--Did the player joined mid game? play the current music and weather going on
-		for p in players.iterate do
-			if gd.lowtime and S_MusicName(p) != gd.currentmusicplaying then
-				S_ChangeGlobalMusic(gd.currentmusicplaying)
-			end
-			if gd.overtime and S_MusicName(p) != gd.currentmusicplaying then
-				S_ChangeGlobalMusic(gd.currentmusicplaying, globalweather, globallevelskynum)
-			end
+		if gd.overtime and S_MusicName(p) != gd.currentmusicplaying then
+			S_ChangeGlobalMusic(gd.currentmusicplaying, globalweather, globallevelskynum)
 		end
 	end
 end
