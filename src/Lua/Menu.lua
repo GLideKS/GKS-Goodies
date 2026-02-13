2ab2d53ecab959ab3ba3b28aceec8ebc54788d4e
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-02-13 13:39:45 -0400

    Update Menu.lua

--- src/Lua/Menu.lua
+++ src/Lua/Menu.lua
@@ -1,3 +1,6 @@
+--Goodies Menu made with MenuLib
+--TO-DO: Complete this menu
+
 local ML = MenuLib
 local buffer = ""
 local bufferid = ML.newBufferID()
@@ -103,7 +106,7 @@ ML.addMenu({
             },
 
             pressFunc = function()
-                print("yes")
+                return
 			end
         })
 
@@ -143,6 +146,8 @@ ML.addMenu({
     end
 })
 
+--Gametype settings
+
 local commands = {
     [1] = {
         text = "Match",
@@ -172,13 +177,28 @@ local commands = {
         tlimit = CV_FindVar("hs_timelimit"),
         plimit = CV_FindVar("hs_pointlimit")
     },
+    [5] = {
+        text = "Default",
+        tlimitcommand = "default_timelimit",
+        plimitcommand = "default_pointlimit",
+        tlimit = CV_FindVar("default_timelimit"),
+        plimit = CV_FindVar("default_pointlimit")
+    },
+    [6] = {
+        text = "Current",
+        tlimitcommand = "timelimit",
+        plimitcommand = "pointlimit",
+        tlimit = CV_FindVar("timelimit"),
+        plimit = CV_FindVar("pointlimit")
+    },
 }
 local copied_value = 0
+
 ML.addMenu({
 	stringId = "gametypesettings",
 	title = "Gametype Settings",
-    width = 250,
-    height = 125,
+    width = 270,
+    height = 180,
 
     drawer = function(v, ML, menu, props)
         local corner_x = props.corner_x + 10
@@ -192,8 +212,12 @@ ML.addMenu({
         for i in pairs(commands) do
             local col = (i - 1) % 2
             local row = (i - 1) / 2
-            local x = corner_x + (col * 115)
+            local x = corner_x + (col * 130)
             local y = corner_y + (row * 40)
+
+            if i > 4 then
+                y = y+40
+            end
 
             v.drawString(x, y, commands[i].text, V_YELLOWMAP)
             v.drawFill(x, y+11, 110, 1, 0)
@@ -311,6 +335,112 @@ ML.addMenu({
             v.draw(paste_pointlimit.x, paste_pointlimit.y, pastepatch)
         end
 
-        v.drawString(corner_x, corner_y+90, "\x82\NOTE: \x80\Changes will apply on the next map", nil, "thin")
+        v.drawString(corner_x, corner_y+85, "\x82\NOTE: \x80\Changes will apply on the next map", nil, "thin")
+        v.drawFill(corner_x, corner_y+105, 240, 1, 150)
     end
 })
+
+--Server messages
+--Disabled atm since menulib doesn't support longer strings lol
+
+/*ML.addMenu({
+	stringId = "servermsg",
+	title = "Server messages",
+    width = 270,
+    height = 180,
+
+    drawer = function(v, ML, menu, props)
+        local corner_x = props.corner_x + 5
+		local corner_y = props.corner_y + 18
+        local btnsize = 10
+        local field_width = menu.width-10
+        local field_color = 29
+
+        --prefix name
+        local sname = {
+            prefix = GKSGoodies.prefixcolors[GKSGoodies.serverprefix.color]..GKSGoodies.serverprefix.text,
+            x = corner_x+3,
+            y = corner_y+3
+        }
+
+        v.drawFill(corner_x, corner_y, field_width, 15, field_color)
+        v.drawString(sname.x, sname.y, "Prefix name", V_ALLOWLOWERCASE)
+        v.drawString(field_width, sname.y, sname.prefix, V_ALLOWLOWERCASE, "right")
+        local edit_prefix = ML.addButton(v, {
+            id = 5,
+            x = field_width+2,
+            y = sname.y-1,
+            width = btnsize,
+            height = btnsize,
+            color = 153,
+            selected = {
+                color = 149
+            },
+
+            pressFunc = function()
+                ML.startTextInput(com_buffer, com_bufferid, {
+                    onenter = function()
+                        ML.client.commandbuffer = "prefix_name "..ML.client.textbuffer
+                    end,
+                    tooltip = {"Set the server's name in the chat"}
+                })
+            end
+        })
+
+        local color_prefix = ML.addButton(v, {
+            id = 5,
+            x = edit_prefix.x+(btnsize+3),
+            y = sname.y-1,
+            width = btnsize,
+            height = btnsize,
+            color = 153,
+            selected = {
+                color = 149
+            },
+
+            pressFunc = function()
+                ML.startTextInput(com_buffer, com_bufferid, {
+                    onenter = function()
+                        ML.client.commandbuffer = "prefix_color "..ML.client.textbuffer
+                    end,
+                    tooltip = {"Type the color for the server's name in the chat";
+                               "See prefix_colorlist for a list of available colors";}
+                })
+            end
+        })
+
+        --Welcome message
+        local welcomemsg = {
+            text = GKSGoodies.welcome.message,
+            sound = GKSGoodies.welcome.sound,
+            x = corner_x+3,
+            y = corner_y+23
+        }
+
+        v.drawFill(corner_x, corner_y+20, field_width, 15, field_color)
+        v.drawString(welcomemsg.x, welcomemsg.y, "Welcome message", V_ALLOWLOWERCASE)
+        v.drawString(field_width, welcomemsg.y+1, (welcomemsg.text):sub(1, 15).."...", V_ALLOWLOWERCASE, "thin-right")
+
+        local edit_welcome = ML.addButton(v, {
+            id = 5,
+            x = field_width+2,
+            y = welcomemsg.y-1,
+            width = btnsize,
+            height = btnsize,
+            color = 153,
+            selected = {
+                color = 149
+            },
+
+            pressFunc = function()
+                ML.startTextInput(com_buffer, com_bufferid, {
+                    onenter = function()
+                        ML.client.commandbuffer = "welcome_message "..ML.client.textbuffer
+                    end,
+                    tooltip = {"Type the welcome message to be shown";
+                               "Current is: "..welcomemsg.text}
+                })
+            end
+        })
+    end
+})*/
