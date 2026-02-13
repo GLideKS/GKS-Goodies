db581acdc6af8b7a4c9547adb9fb931738ac7ab4
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-02-13 15:20:58 -0400

    Use ShouldDamage hook for race damage prevention

--- src/Lua/Race/Race Adjustments.lua
+++ src/Lua/Race/Race Adjustments.lua
@@ -1,15 +1,11 @@
 local GoodiesHook = GoodiesHook
 
---Prevent damage from other players on countdown
-GoodiesHook.PlayerThink.RaceCountdownNoDMG = function(p)
+--Prevent damage on countdown
+local function RaceCountdownNoDMG(mo)
 	if not (gametyperules & GTR_RACE) then return end
-	if not (p and p.mo and p.mo.valid) then return end
-	if (leveltime < 4*TICRATE) or (p.realtime == 0) then
-		if not (p.mo.flags & MF_NOCLIPTHING) then
-			p.mo.flags = $|MF_NOCLIPTHING
-		end
-	elseif (p.mo.flags & MF_NOCLIPTHING)
-	and leveltime == (4*TICRATE)+5 then
-		p.mo.flags = $ &~MF_NOCLIPTHING
+	if not (mo and mo.valid) then return end
+	if (leveltime < 4*TICRATE) or (mo.player.realtime == 0) then
+		return false
 	end
 end
+addHook("ShouldDamage", RaceCountdownNoDMG, MT_PLAYER)
