44904bc1efd25c4d15de7155605e543f37d49c70
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-02-23 11:34:52 -0400

    Globalize team color variants

--- src/Lua/Team GT Visuals.lua
+++ src/Lua/Team GT Visuals.lua
@@ -1,39 +1,41 @@
 local GoodiesHook = GoodiesHook
 
---List of blue color variants for the Blue Team
-local blue_variants = {
-	SKINCOLOR_WAVE,
-	SKINCOLOR_COBALT,
-	SKINCOLOR_SAPPHIRE,
-	SKINCOLOR_CORNFLOWER,
-	SKINCOLOR_BLUE,
-	SKINCOLOR_GALAXY,
-	SKINCOLOR_DAYBREAK,
-	SKINCOLOR_CERULEAN,
-	SKINCOLOR_MARINE,
-	SKINCOLOR_SKY,
-	SKINCOLOR_OCEAN
-}
-
---List of red color variants for the Red Team
-local red_variants = {
-	SKINCOLOR_RUBY,
-	SKINCOLOR_CHERRY,
-	SKINCOLOR_SALMON,
-	SKINCOLOR_PEPPER,
-	SKINCOLOR_RED,
-	SKINCOLOR_CRIMSON,
-	SKINCOLOR_GARNET,
-	SKINCOLOR_KETCHUP
+GKSGoodies.TeamColors = {
+	--List of blue color variants for the Blue Team
+	blue = {
+		SKINCOLOR_WAVE,
+		SKINCOLOR_COBALT,
+		SKINCOLOR_SAPPHIRE,
+		SKINCOLOR_CORNFLOWER,
+		SKINCOLOR_BLUE,
+		SKINCOLOR_GALAXY,
+		SKINCOLOR_DAYBREAK,
+		SKINCOLOR_CERULEAN,
+		SKINCOLOR_MARINE,
+		SKINCOLOR_SKY,
+		SKINCOLOR_OCEAN
+	},
+	--List of red color variants for the Red Team
+	red = {
+		SKINCOLOR_RUBY,
+		SKINCOLOR_CHERRY,
+		SKINCOLOR_SALMON,
+		SKINCOLOR_PEPPER,
+		SKINCOLOR_RED,
+		SKINCOLOR_CRIMSON,
+		SKINCOLOR_GARNET,
+		SKINCOLOR_KETCHUP
+	}
 }
 
 --Change the colors on player spawn
 GoodiesHook.PlayerSpawn.TeamColorVariant = function(p)
 	if not (gametyperules & GTR_TEAMS) then return end
+	local t = GKSGoodies.TeamColors
 	if p.ctfteam == 1 then
-		p.gd_redvariant = red_variants[P_RandomRange(1, #red_variants)]
+		p.gd_redvariant = t.red[P_RandomRange(1, #t.red)]
 	elseif p.ctfteam == 2 then
-		p.gd_bluevariant = blue_variants[P_RandomRange(1, #blue_variants)]
+		p.gd_bluevariant = t.blue[P_RandomRange(1, #t.blue)]
 	end
 end
 
