6b4514ebabe8772bd5d9b64e129d404bfac542ab
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-02-23 12:18:40 -0400

    Add spawn object command

--- src/Lua/Commands.lua
+++ src/Lua/Commands.lua
@@ -196,3 +196,19 @@ COM_AddCommand("welcome_message", function(p, message)
 		CONS_Printf(p, "Sets a welcome message for the joining player")
 	end
 end, COM_ADMIN)
+
+--Admin funnies
+
+COM_AddCommand("gd_spawnobject", function(p, object) --Spawn object
+	if not object then
+		CONS_Printf(p, "Spawns an object in front of the player")
+		CONS_Printf(p, "See https://wiki.srb2.org/wiki/List_of_Object_types for a list of valid object types")
+		CONS_Printf(p, "")
+		CONS_Printf(p, "Example: ")
+		CONS_Printf(p, "gd_spawnobject MT_REDCRAWLA")
+	else
+		local x, y = cos(p.realmo.angle),sin(p.realmo.angle)
+		local obj = P_SpawnMobjFromMobj(p.realmo, (90*x), (90*y), 0, _G[object])
+		obj.angle = p.realmo.angle
+	end
+end, COM_ADMIN)
