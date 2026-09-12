b9f6b7416bd6ef0aee3b8118db5da06a3b9e0709
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-09-12 01:56:23 -0400

    Do not allow ring sharing on special stages

--- src/Lua/Coop/Ring sharing.lua
+++ src/Lua/Coop/Ring sharing.lua
@@ -38,6 +38,7 @@ mobjinfo[MT_RINGSHARE] = { -- The Ring to share with players.
 local function RingShare(p) -- Player's ring sharing functionality
     if not rshare.value then return end
     if not G_CoopGametype() then return end -- Only Co-Op
+    if G_IsSpecialStage() then return end -- This is like, sabotage on special stages lol
     if p.spectator then return end -- I saw cases...
     if not p.rings then return end
     local mo = p.mo
