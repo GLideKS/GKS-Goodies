6449566e95a1120bc41b8cf27a1013b9311f72e9
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-09-23 19:28:32 -0400

    [CustomExit] Remove forced optional arg
    Will run like it's forced now. Not that much a performance impact on a thinkframe tbh

--- src/Lua/Utilities/map_customexits.lua
+++ src/Lua/Utilities/map_customexits.lua
@@ -2,12 +2,10 @@
 
 -- [[ Set up the map's next level, the code behavior will rely on this. ]] --
 
-local custom_exits = {} -- sets a custom exit on map load
-local force_customexit = {} -- sets a custom exit when there's enough players. Useful to override map's custom exit as well.
+local custom_exits = {} -- List of maps that will contain a custom exit.
 
-addHook("NetVars", function (net) -- Should be synched in the netgames
+addHook("NetVars", function(net) -- Should be synched in the netgames
     custom_exits = net($)
-    force_customexit = net($)
 end)
 
 local function SetCustomExitFromMap(map, skipstats)
@@ -22,18 +20,15 @@ end
 
 -- [[ Commands ]] --
 
-COM_AddCommand("custommapexit", function(p, map, nextmap, force)
-    local yes = "yes" or "1" or "true" or "on"
-    if force == yes then force = true end
+COM_AddCommand("custommapexit", function(p, map, nextmap)
     map = tonumber(map)
     nextmap = tonumber(nextmap)
 
     if map == nil then -- Find the map
-        CONS_Printf(p, "\nChanges the <map>'s next level. Once completion, the map changes to the <nextmap>'s.")
-        CONS_Printf(p, "if <force> is on, will override the custom next level in the map if any.\n")
+        CONS_Printf(p, "\nChanges the <map>'s next level. Once completion, the map changes to the <nextmap>'s.\n")
         CONS_Printf(p, "NOTE: Map IDs must be in extended map numbers\n")
-        CONS_Printf(p, "EXAMPLE: custommapexit <map> <nextmap> <force>")
-        CONS_Printf(p, "EXAMPLE: custommapexit 01 05 true\n")
+        CONS_Printf(p, "EXAMPLE: custommapexit <map> <nextmap>")
+        CONS_Printf(p, "EXAMPLE: custommapexit 01 05\n")
         return
     elseif not G_FindMapByNameOrCode(map) then -- Is a valid map?
         CONS_Printf(p, "\x85".."Invalid map.")
@@ -58,10 +53,6 @@ COM_AddCommand("custommapexit", function(p, map, nextmap, force)
     if gamemap == map then -- If the map given is the same one we're in, apply directly.
         SetCustomExitFromMap(map)
     end
-
-    if force then -- If force, will override the custom next level in the map if any
-        force_customexit[map] = true
-    end
 end, COM_ADMIN)
 
 COM_AddCommand("custommapexit_remove", function(p, map) -- Seems weird when trying to remove a custom exit from the list so betet
@@ -80,10 +71,6 @@ COM_AddCommand("custommapexit_remove", function(p, map) -- Seems weird when tryi
 
     custom_exits[map] = nil  -- Remove it from the list
 
-    if force_customexit[map] then
-        force_customexit[map] = nil -- Remove it from the forced list if available
-    end
-
     if gamemap == map then -- If the map given is the same one we're in, apply directly.
         G_SetCustomExitVars(mapheaderinfo[gamemap].nextlevel)
     end
@@ -95,10 +82,6 @@ COM_AddCommand("custommapexit_clear", function(p) -- Seems weird when trying to 
     for i,v in pairs(custom_exits) do -- According to Jisk, net synched tables should be removed like this.
         custom_exits[i] = nil
     end
-
-    for i,v in pairs(force_customexit) do
-        force_customexit[i] = nil
-    end
     CONS_Printf(p, "\x83".."Custom exits has been cleared sucessfully.")
 end, COM_ADMIN)
 
@@ -107,9 +90,8 @@ COM_AddCommand("custommapexit_list", function(p)
 
     for map, nextmap in pairs(custom_exits) do
         local string = IDAndName(map).." --> "..IDAndName(nextmap)
-        local forced = force_customexit[map] and "\x83".." (Forced: Yes)" or "\x85".." (Forced: No)"
 
-        CONS_Printf(p, string..forced)
+        CONS_Printf(p, string)
     end
 end, COM_ADMIN)
 
@@ -117,12 +99,10 @@ end, COM_ADMIN)
 
 local function CustomExitOverride()
     if gamestate != GS_LEVEL then return end
-    if not force_customexit[gamemap] then return end
     if not custom_exits[gamemap] then return end
     if not G_EnoughPlayersFinished() then return end
 
     SetCustomExitFromMap(gamemap)
 end
 
-addHook("MapLoad", SetCustomExitFromMap)
 addHook("ThinkFrame", CustomExitOverride)
